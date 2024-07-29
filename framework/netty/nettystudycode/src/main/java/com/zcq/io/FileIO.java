ab66f2c1ee29215807e2721b162dd3986c59b7be
zhangchunqi <zhangchunqi@xkw.cn>
2024-07-29 15:03:32 +0800

    netty源码学习-类加载器加载文件

--- framework/netty/nettystudycode/src/main/java/com/zcq/io/FileIO.java
+++ framework/netty/nettystudycode/src/main/java/com/zcq/io/FileIO.java
@@ -2,12 +2,18 @@ package com.zcq.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 
 public class FileIO {
 
     public static void main(String[] args) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("D:\\code\\nettystudycode\\src\\main\\resources\\fileio.txt");
+
+        //get classpath
+        URL resource = FileIO.class.getClassLoader().getResource("fileio.txt");
+        String file = resource.getFile();
+        FileInputStream fileInputStream = new FileInputStream(file);
         FileChannel channel = fileInputStream.getChannel();
         int read = fileInputStream.read();
         System.out.println(read);

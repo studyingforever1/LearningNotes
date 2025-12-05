129853d705a77c69148606c36b912548fa6f5ff6
zhangchunqi <zhangchunqi@xkw.cn>
2025-12-05 09:51:33 +0800

    springboot源码-初始化

---
+++ framework/springboot/springbootcodestudy/src/main/java/com/zcq/springbootcodestudy/SpringbootcodestudyApplication.java
@@ -0,0 +1,13 @@
+package com.zcq.springbootcodestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcodestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcodestudyApplication.class, args);
+    }
+
+}
---
+++ framework/springboot/springbootcodestudy/src/test/java/com/zcq/springbootcodestudy/SpringbootcodestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcq.springbootcodestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcodestudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c2c36c06965ca4be5801214c5097e4f8285bc9d1
zhangchunqi <zhangchunqi@xkw.cn>
2025-12-19 15:06:44 +0800

    springwebflux学习-初始化

---
+++ framework/springwebflux/springwebfluxstudy/src/main/java/com/zcq/springwebfluxstudy/SpringwebfluxstudyApplication.java
@@ -0,0 +1,13 @@
+package com.zcq.springwebfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebfluxstudyApplication.class, args);
+    }
+
+}

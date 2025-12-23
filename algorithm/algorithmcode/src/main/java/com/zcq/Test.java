c6569b551b40ba70d512fdcdcb1f9d2b0ba2044d
zhangchunqi <zhangchunqi@xkw.cn>
2025-12-23 11:27:33 +0800

    力扣算法学习-线段树

--- algorithm/algorithmcode/src/main/java/com/zcq/Test.java
+++ algorithm/algorithmcode/src/main/java/com/zcq/Test.java
@@ -6,100 +6,99 @@ public class Test {
     }
 }
 
-class MyCalendar {
+class MyCalendarThree {
 
     SegmentTree segmentTree;
 
-    public MyCalendar() {
-        segmentTree = new SegmentTree(0, Integer.MAX_VALUE, 0);
+    public MyCalendarThree() {
+        segmentTree = new SegmentTree(0, 1000000000, 0);
     }
 
-    public boolean book(int startTime, int endTime) {
-        if (segmentTree.query(startTime, endTime - 1) > 0) {
-            return false;
-        }
-        segmentTree.updateRange(startTime, endTime - 1, 1);
-        return true;
+    public int book(int startTime, int endTime) {
+        segmentTree.addRange(startTime, endTime - 1, 1);
+        return segmentTree.query(0, 100_000_000);
     }
 }
 
 class SegmentTree {
     public static class SegmentNode {
-        int l, r;
-        int sum;
-        SegmentNode left, right;
+        private int l, r;
+        private SegmentNode left, right;
+        private int max;
 
-        boolean hasLazyAssign;
-        int lazyAssign;
+        private boolean hasLazyAdd;
+        private int lazyAdd;
 
-        public SegmentNode(int l, int r, int sum) {
+        public SegmentNode(int l, int r, int max) {
             this.l = l;
             this.r = r;
-            this.sum = sum;
-            this.hasLazyAssign = false;
+            this.max = max;
         }
     }
 
     SegmentNode root;
     int defaultValue;
 
-    public SegmentTree(int start, int end, int defaultValue) {
-        this.defaultValue = defaultValue;
-        root = new SegmentNode(start, end, defaultValue);
+    public SegmentTree(int l, int r, int defaultValue) {
+        root = new SegmentNode(l, r, defaultValue);
     }
 
-    private void initChildNodeIfNeed(SegmentNode node) {
-        if (node.l == node.r) {
+    public void addRange(int qL, int qR, int value) {
+        addRange(root, qL, qR, value);
+    }
+
+    private void addRange(SegmentNode root, int qL, int qR, int value) {
+        if (root.l >= qL && root.r <= qR) {
+            root.hasLazyAdd = true;
+            root.lazyAdd += value;
+            root.max += value;
             return;
         }
-        int mid = node.l + (node.r - node.l) / 2;
-        if (node.left == null) {
-            node.left = new SegmentNode(node.l, mid, defaultValue);
-        }
-        if (node.right == null) {
-            node.right = new SegmentNode(mid + 1, node.r, defaultValue);
-        }
-    }
+        buildChildIfNeed(root);
+        pushDown(root);
 
-    public void updateRange(int qL, int qR, int value) {
-        updateRange(root, qL, qR, value);
-    }
-
-    private void updateRange(SegmentNode node, int qL, int qR, int value) {
-        if (node.l >= qL && node.r <= qR) {
-            node.hasLazyAssign = true;
-            node.sum = (node.r - node.l + 1) * value;
-            node.lazyAssign = value;
-            return;
-        }
-        initChildNodeIfNeed(node);
-        pushDown(node);
-
-        int mid = node.l + (node.r - node.l) / 2;
-        if (mid >= qR) {
-            updateRange(node.left, qL, qR, value);
-        } else if (mid < qL) {
-            updateRange(node.right, qL, qR, value);
+        int mid = root.l + (root.r - root.l) / 2;
+        if (qR <= mid) {
+            addRange(root.left, qL, qR, value);
+        } else if (qL > mid) {
+            addRange(root.right, qL, qR, value);
         } else {
-            updateRange(node.left, qL, mid, value);
-            updateRange(node.right, mid + 1, qR, value);
+            addRange(root.left, qL, mid, value);
+            addRange(root.right, mid + 1, qR, value);
         }
-        node.sum = node.left.sum + node.right.sum;
+        root.max = Math.max(root.left.max, root.right.max);
+
     }
 
-    private void pushDown(SegmentNode node) {
-        if (!node.hasLazyAssign) {
+    private void pushDown(SegmentNode root) {
+        if (!root.hasLazyAdd) {
             return;
         }
-        node.left.hasLazyAssign = true;
-        node.left.sum = (node.left.r - node.left.l + 1) * node.lazyAssign;
-        node.left.lazyAssign = node.lazyAssign;
 
-        node.right.hasLazyAssign = true;
-        node.right.sum = (node.right.r - node.right.l + 1) * node.lazyAssign;
-        node.right.lazyAssign = node.lazyAssign;
+        root.left.hasLazyAdd = true;
+        root.left.lazyAdd += root.lazyAdd;
+        root.left.max += root.lazyAdd;
 
-        node.hasLazyAssign = false;
+        root.right.hasLazyAdd = true;
+        root.right.lazyAdd += root.lazyAdd;
+        root.right.max += root.lazyAdd;
+
+        root.hasLazyAdd = false;
+        root.lazyAdd = 0;
+
+    }
+
+    private void buildChildIfNeed(SegmentNode root) {
+        if (root.l == root.r) {
+            return;
+        }
+        int mid = root.l + (root.r - root.l) / 2;
+        if (root.left == null) {
+            root.left = new SegmentNode(root.l, mid, defaultValue);
+        }
+        if (root.right == null) {
+            root.right = new SegmentNode(mid + 1, root.r, defaultValue);
+        }
     }
 
     public int query(int qL, int qR) {
@@ -107,19 +106,22 @@ class SegmentTree {
     }
 
     private int query(SegmentNode root, int qL, int qR) {
-        if (root.l >= qL && root.r <= qR) {
-            return root.sum;
+        if (qL <= root.l && root.r <= qR) {
+            return root.max;
         }
-        initChildNodeIfNeed(root);
+
+        buildChildIfNeed(root);
         pushDown(root);
+
         int mid = root.l + (root.r - root.l) / 2;
-        if (mid >= qR) {
+        if (qR <= mid) {
             return query(root.left, qL, qR);
-        } else if (mid < qL) {
+        } else if (qL > mid) {
             return query(root.right, qL, qR);
         } else {
-            return query(root.left, qL, mid) + query(root.right, mid + 1, qR);
+            return Math.max(query(root.left, qL, mid), query(root.right, mid + 1, qR));
         }
     }
 
 }
+

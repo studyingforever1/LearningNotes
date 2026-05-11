6f967dba0c045977b0436bc6b8ea9c7ec94b04d2
zhangchunqi <1024260051@qq.com>
2026-05-11 15:55:57 +0800

    提交

--- algorithm/algorithmcode/src/main/java/com/zcq/Test.java
+++ algorithm/algorithmcode/src/main/java/com/zcq/Test.java
@@ -1,72 +1,42 @@
 package com.zcq;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Test {
     public static void main(String[] args) {
-    }
-}
+        int[] ints = {3, 4, 2, 1, 9, 7};
+        mergeSort(ints, 0, ints.length - 1);
+        System.out.println(Arrays.toString(ints));
 
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int p = leftBound(arr, x);
-        int p1 = p, p2 = p;
-        if (p == arr.length) {
-            p1 = p - 1;
-        }
-        List<Integer> res = new ArrayList<>();
-        while (p1 >= 0 && p2 < arr.length && res.size() < k) {
-            if (p1 == p2) {
-                if (arr[p1] == x) {
-                    res.add(arr[p1]);
-                    p1--;
-                    p2++;
-                } else {
-                    p1--;
-                }
-            } else {
-                if (Math.abs(arr[p1] - x) < Math.abs(arr[p2] - x)) {
-                    res.add(arr[p1]);
-                    p1--;
-                } else if (Math.abs(arr[p1] - x) > Math.abs(arr[p2] - x)) {
-                    res.add(arr[p2]);
-                    p2++;
-                } else {
-                    res.add(arr[p1]);
-                    p1--;
-                }
-            }
-        }
-        if (p1 < 0) {
-            while (p2 < arr.length && res.size() < k) {
-                res.add(arr[p2]);
-                p2++;
-            }
-        }
-        if (p2 >= arr.length) {
-            while (p1 >= 0 && res.size() < k) {
-                res.add(arr[p1]);
-                p1--;
-            }
-        }
-        res.sort(Comparator.naturalOrder());
-        return res;
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>(16,0.75f,true);
     }
 
-    private int leftBound(int[] arr, int x) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == x) {
-                right = mid - 1;
-            } else if (arr[mid] < x) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    private static void mergeSort(int[] ints, int l, int r) {
+        if (l >= r) {
+            return;
         }
-        return left;
+        int mid = l + (r - l) / 2;
+        mergeSort(ints, l, mid);
+        mergeSort(ints, mid + 1, r);
+        merge(ints, l, r, mid);
     }
+
+    private static void merge(int[] ints, int l, int r, int mid) {
+        int[] temp = Arrays.copyOfRange(ints, l, r + 1);
+        int i = 0, j = mid - l + 1, k = l;
+        while (i <= mid - l && j <= r - l) {
+            ints[k++] = temp[i] < temp[j] ? temp[i++] : temp[j++];
+        }
+        while (i <= mid - l) {
+            ints[k++] = temp[i++];
+        }
+        while (j <= r - l) {
+            ints[k++] = temp[j++];
+        }
+
+    }
+
+
 }

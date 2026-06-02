67ea289a53d24984940e52bdcd1931f796e72961
zhangchunqi <1024260051@qq.com>
2026-06-02 15:28:09 +0800

    提交

--- algorithm/algorithmcode/src/main/java/com/zcq/Test.java
+++ algorithm/algorithmcode/src/main/java/com/zcq/Test.java
@@ -1,33 +1,8 @@
 package com.zcq;
 
-import java.util.LinkedList;
 
 public class Test {
     public static void main(String[] args) {
     }
 }
 
-class Solution {
-    LinkedList<Integer> path = new LinkedList<>();
-    int res = 0;
-    public int sumNumbers(TreeNode root) {
-        traverse(root);
-        return res;
-    }
-
-    private void traverse(TreeNode root) {
-        if (root == null) return;
-        path.add(root.val);
-        if (root.left == null && root.right == null) {
-            int sum = 0;
-            for (Integer i : path) {
-                sum += sum * 10 + i;
-            }
-            System.out.println(sum);
-            res += sum;
-        }
-        traverse(root.left);
-        traverse(root.right);
-        path.removeLast();
-    }
-}
